36ebf85b2df13cf087f5a2314c72b2d5c54d0f2e
Marco <modasso23@icloud.com>
2025-05-07 11:05:14 +0200

    Adding a subfunction we'll need to implement and starting describing some functions we'll implement in the file Utils.hpp

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Code and documentation for the PCS project 2025
 
 Domande:
 1. Il file .txt può essere un .csv oppure un file che ha degli spazi come delimitatori?
+2. Dobbiamo solo fare in modo che il programma dia output solo per poliedri che hanno p = 3, così come dia il poliedro di Goldberg solo per quelli con q = 3 ?
 
 Cose da fare:
 1. Creare algoritmo che crea il poliedro a partire da p e q;
 2. Creare algoritmo che ne fa la tassellazione a seconda della classe;
+   - Creare una funzione che calcola i baricentri dei poligoni che sono facce del poliedro.
 3. Creare algoritmo che fa il duale di tale tassellazione;
 4. Creare una funzione che proietta sulla sfera;
 5. Fare documentazione UML delle strutture e del coldice;
--- src/Utils.hpp
+++ src/Utils.hpp
@@ -5,6 +5,40 @@
 using namespace PolyhedraLibrary;
 
 /* CreatePolyhedron is a function that creates the Polyhedron starting from the input data p and q
-p: number of vertices of each face of the polyhedron
-q: valence of the polyhedron */
+Inputs list:
+p: number of vertices of each face of the polyhedron; therefore, each face is a p-sided polygon
+q: number of regular p-sided polygon faces around each vertex of the polyhedra*/
 Polyhedron CreatePolyhedron(int& p, int& q);
+
+/* TypeITassellation is a function that modifies the attributes of the object <polyhedron> in order to 
+output its tassellations of type I regarding geodetic polyhedra 
+Inputs list:
+polyhedron: object of type "Polyhedron" that the function modifies. */
+void TypeITassellation(Polyhedron& polyhedron);
+
+/* TypeIITassellation is a function that modifies the attributes of the object <polyhedron> in order to 
+output its tassellations of type II regarding geodetic polyhedra 
+Inputs list:
+polyhedron: object of type "Polyhedron" that the function modifies. */
+void TypeIITassellation(Polyhedron& polyhedron);
+
+/* Dualise is a function that outputs the dual, expressed as the type "Polyhedron", of a <polyhedron> given as input 
+Inputs list:
+polyhedron: object of type "Polyhedron" that the function uses to create its dual */
+Polyhedron Dualise(Polyhedron& polyhedron);
+// Che sia meglio dare come output il tipo void? Se facciamo return Polyhedron sarà una grossa e lenta operazione, vero? 
+// Forse è meglio definire un nuovo poliedro prima di chiamare questa funzione e mettiamo come altro input della funzione il poliedro stesso in cui dobbiamo fare output?
+
+/* OntoTheSphere is a function that takes a point as input and projects it onto the unitary sphere 
+Inputs list:
+vertex: the point that the function will projects onto the sphere normalising its vector*/
+void OntoTheSphere(vertex& vertex);
+// Pensavo di usare la normalizzazione per proiettare sulla sfera unitaria, a voi viene in mente un modo migliore? Pensate che il mio funzioni?
+// La variabile vertex ha come tipo vertex, ma in realtà non so ancora cosa sarà, probabilmente un vettore.
+
+/* WhichIsTheMinimumPathBetween is a function that calculates the minimum path between the two vertices marked 
+by the ids given as inputs 
+Inputs list:
+id_vertex_1: the id associated to the first vertex of the minimum path we're looking for 
+id_vertex_2: the id associated to the second/last vertex of the minimum path we're looking for*/
+Path WhichIsTheMinimumPathBetween(int& id_vertex_1, int& id_vertex_2);
