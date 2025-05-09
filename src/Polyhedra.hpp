aa0b44b6b727de2686a70dd03a8cb0faa44a9e15
ManMac32i <manuel.macis@icloud.com>
2025-05-09 12:54:37 +0200

    Struct Polyhedra and initialization Polyhedra

--- src/Polyhedra.hpp
+++ src/Polyhedra.hpp
@@ -1,10 +1,29 @@
 /* File where we'll create the class or struct Polyhedra with all of its attributes */
 
-#pragma once
+# pragma once
+# include <vector>
+# include "Eigen/Eigen"
 
 namespace PolyhedraLibrary{
 
-    struct Polyhedron{
+    struct GEOPolyhedron
+    {
+        int NumVertices; // Number of vertices
+        std::vector<int> IdVertices; // Id of all vertices
+        Eigen::MatrixXd CoordVertices; // Coordinates of the vertices, 2 x NumVertices matrix
+
+        int NumEdges; // Number of Edges
+        std::vector<int> IdEdges; // Id of all vertices
+        Eigen::MatrixXi ExtremesEdges; // Extremes of each edge, 2 x NumEdges matrix
+
+        int NumFaces; // Number of faces 
+        int VertFaces; // Number of vertices per face
+        std::vector<int> IdFaces; // Id of all faces
+        Eigen::MatrixXi ListVertFaces; // All the vertices of each face, VertFaces x NumFaces matrix
+    };
+    /* TODO Path can be useful if we want a particular structure for the minimum path between
+    two vertices */
+    struct Path {
 
     };
 
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,1 +1,41 @@
 /* File containing the body of the functions we'll use */
+
+# pragma once
+# include "Polyhedra.hpp"
+# include <vector>
+
+using namespace PolyhedraLibrary;
+using namespace std;
+
+GEOPolyhedron CreatePolyhedron(const int& p, const int& q)
+{
+    GEOPolyhedron polyhedron;
+    polyhedron.NumFaces = 4 * q / ((p - 2) * (q - 2) - 4);
+    polyhedron.NumEdges = p * polyhedron.NumFaces / 2;
+    polyhedron.NumVertices = p * polyhedron.NumFaces / q;
+}
+
+void TypeITassellation(GEOPolyhedron& polyhedron)
+{
+
+}
+
+void TypeIITassellation(GEOPolyhedron& polyhedron)
+{
+
+}
+
+GEOPolyhedron Dualise(GEOPolyhedron& polyhedron)
+{
+
+}
+
+void OntoTheSphere(vector<double>& vertex)
+{
+
+}
+
+Path WhichIsTheMinimumPathBetween(int& id_vertex_1, int& id_vertex_2)
+{
+
+}
